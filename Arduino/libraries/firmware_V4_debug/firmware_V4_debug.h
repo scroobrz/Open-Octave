a0858ac171c716bb7525dacbeae6c1eca6510d9b
scroobrz <robinparker07uk@gmail.com>
2026-02-19 21:33:17 +0000

    refactored custom printf function which is no longer necessary

--- Arduino/libraries/firmware_V4_debug/firmware_V4_debug.h
+++ Arduino/libraries/firmware_V4_debug/firmware_V4_debug.h
@@ -2,7 +2,6 @@
 #define FIRMWARE_V4_DEBUG_H
 
 // #include <WebSerial.h>  // WebSerial disabled until AsyncWebServer migration
-#include <stdarg.h>
 
 // ============ DEBUG CONFIGURATION ============
 
@@ -27,82 +26,8 @@
 #define LOG_VAL(x)   { Serial.print(x); }
 #define LOGLN_VAL(x) { Serial.println(x); }
 
-// Lightweight printf that reads format string from flash
-inline void LOGF_P(const char *str_P, ...) {
-  va_list args;
-  va_start(args, str_P);
-
-  char c;
-  while ((c = pgm_read_byte(str_P++))) {
-    if (c == '%') {
-      c = pgm_read_byte(str_P++);
-
-      // Check for 'l' modifier (long)
-      bool isLong = false;
-      if (c == 'l') {
-        isLong = true;
-        c = pgm_read_byte(str_P++);
-      }
-
-      switch (c) {
-      case 'd': // Signed decimal
-      case 'i':
-        if (isLong) {
-          Serial.print(va_arg(args, long));
-        } else {
-          Serial.print(va_arg(args, int));
-        }
-        break;
-
-      case 'u': // Unsigned decimal
-        if (isLong) {
-          Serial.print(va_arg(args, unsigned long));
-        } else {
-          Serial.print(va_arg(args, unsigned int));
-        }
-        break;
-
-      case 'x': // Hex lowercase
-      case 'X': // Hex uppercase
-        if (isLong) {
-          Serial.print(va_arg(args, unsigned long), HEX);
-        } else {
-          Serial.print(va_arg(args, unsigned int), HEX);
-        }
-        break;
-
-      case 's': // String (from RAM)
-        Serial.print(va_arg(args, const char *));
-        break;
-
-      case 'c': // Character
-        Serial.print((char)va_arg(args, int));
-        break;
-
-      case '%': // Literal %
-        Serial.print('%');
-        break;
-
-      default: // Unknown - print as-is
-        Serial.print('%');
-
-        if (isLong) {
-          Serial.print('l');
-        }
-        
-        Serial.print(c);
-        break;
-      }
-    } else {
-      Serial.print(c);
-    }
-  }
-
-  va_end(args);
-}
-
-// Macro to automatically wrap format strings with PSTR() for Flash storage
-#define LOGF(str, ...) LOGF_P(PSTR(str), ##__VA_ARGS__)
+// Print formatted strings
+#define LOGF(str, ...) Serial.printf(str, ##__VA_ARGS__)
 
 #else
 
