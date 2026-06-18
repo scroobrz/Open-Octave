0ed5d116038f0d3127db44b7f56054b9827515a6
Robin Campbell-Parker <robinparker07uk@gmail.com>
2026-06-18 16:48:58 +0100

    chore: change port for frontend

--- software/web/vite.config.js
+++ software/web/vite.config.js
@@ -21,8 +21,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
+    port: 5175,
+    strictPort: false,
     proxy: {
       '/api': 'http://127.0.0.1:3000',
     },
